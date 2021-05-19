f32412dc952e2e348d0e9e95084657ca14cfa999
Alberto De Pena <albertodepena89@gmail.com>
2021-05-18 19:20:00 -0500

    Working on unit tests

--- Tests/MaybeTests.cs
+++ Tests/MaybeTests.cs
@@ -6,7 +6,7 @@ namespace FunctionalCsharp.Tests
     public class MaybeTests
     {
         [Fact]
-        public void TestOne()
+        public void NothingIsNothing()
         {
             var maybe = Maybe<int>.Nothing();
 
@@ -14,11 +14,59 @@ namespace FunctionalCsharp.Tests
         }
 
         [Fact]
-        public void TestTwo()
+        public void SomethingIsSomething()
         {
-            var maybe = Maybe<int>.Something(10);
+            var maybe = Maybe<int>.Something(1);
 
             Assert.True(maybe.IsSomething);
+        }
+
+        [Fact]
+        public void EmptyMaybeObeysFirstFunctorLaw()
+        {
+            Func<string, string> id = x => x;
+            var maybe = Maybe<string>.Nothing();
+
+            Assert.Equal(maybe, maybe.Map(id));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("foo")]
+        [InlineData("bar")]
+        [InlineData("corge")]
+        [InlineData("antidisestablishmentarianism")]
+        public void PopulatedMaybeObeysFirstFunctorLaw(string value)
+        {
+            Func<string, string> id = x => x;
+            var maybe = Maybe<string>.Something(value);
+
+            Assert.Equal(maybe, maybe.Map(id));
+        }
+
+        [Fact]
+        public void EmptyMaybeObeysSecondFunctorLaw()
+        {
+            Func<string, int> g = s => s.Length;
+            Func<int, bool> f = i => i % 2 == 0;
+            var maybe = Maybe<string>.Nothing();
+
+            Assert.Equal(maybe.Map(g).Map(f), maybe.Map(s => f(g(s))));
+        }
+
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("foo", false)]
+        [InlineData("bar", false)]
+        [InlineData("corge", false)]
+        [InlineData("antidisestablishmentarianism", true)]
+        public void PopulatedMaybeObeysSecondFunctorLaw(string value, bool expected)
+        {
+            Func<string, int> g = s => s.Length;
+            Func<int, bool> f = i => i % 2 == 0;
+            var maybe = Maybe<string>.Something(value);
+
+            Assert.Equal(maybe.Map(g).Map(f), maybe.Map(s => f(g(s))));
         }
     }
 }
